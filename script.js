8fcc9ada597a780130327fea3fe9a1aa83f68c0c
Dart <example@gmail.com>
2022-12-09 22:18:49 +0200

    ex1

--- script.js
+++ script.js
@@ -12,15 +12,16 @@ const personalMovieDB = {
 
     
 
-
-
-
 for (let i = 0; i < 2; i++) {
     const   a = prompt ('Один из последних просмотренных фильмов?', ''),
             b = prompt ('На сколько оцените его?','');
 
 personalMovieDB.movies [a] = b;
 }
+
+
+
+
 
 console.log(personalMovieDB);
 // blablabla
